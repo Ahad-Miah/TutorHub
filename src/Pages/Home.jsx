8e1dd536f4fb08b8531802aed9c9768dd837da24
Ahad-Miah <ahadahmedcc@gmail.com>
2024-12-22 02:37:57 +0600

    added stat on hompage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Banner from '../Components/Banner/Banner';
+import Stat from '../Components/Stat/Stat';
 
 const Home = () => {
     return (
         <div className=''>
+            <section>
             <Banner></Banner>
+            </section>
+            <section>
+            <Stat></Stat>
+            </section>
         </div>
     );
 };
