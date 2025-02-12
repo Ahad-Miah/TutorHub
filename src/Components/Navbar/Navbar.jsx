455ad189ce8bace6103c07be3f89ad6a2645ad9d
Ahad-Miah <ahadahmedcc@gmail.com>
2025-02-12 16:04:32 +0600

    added dropwown in navLinks

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,19 +5,20 @@ import { IoIosSunny } from "react-icons/io";
 import { FiMoon } from "react-icons/fi";
 import { toast } from 'react-toastify';
 import 'react-tooltip/dist/react-tooltip.css'
+import { MdKeyboardArrowDown } from "react-icons/md";
 import { Tooltip } from 'react-tooltip';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const navigate=useNavigate();
+    const navigate = useNavigate();
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
 
     // theme toogle
-    const [themeToggle,setThemeToggle]=useState(true);
-    const [theme,setTheme]=useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light");
+    const [themeToggle, setThemeToggle] = useState(true);
+    const [theme, setTheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light");
 
     useEffect(() => {
         localStorage.setItem("theme", theme);
@@ -26,14 +27,14 @@ const Navbar = () => {
 
     }, [theme]);
 
-    const handleToggle=()=>{
+    const handleToggle = () => {
         setThemeToggle(!themeToggle);
-        
-        if(themeToggle){
+
+        if (themeToggle) {
             setTheme("dark");
             toast.success(`Theme changed to dark`)
         }
-        else{
+        else {
             setTheme("light");
             toast.success(`Theme changed to light`)
         }
@@ -41,21 +42,21 @@ const Navbar = () => {
     const { signout, user, loading } = useContext(AuthContext);
     // console.log(user)
     // sign out
-    const handleSignout=()=>{
+    const handleSignout = () => {
         signout()
-        .then(() => {
-            navigate('/login');
-        })
-        .catch(err => console.log(err));
+            .then(() => {
+                navigate('/login');
+            })
+            .catch(err => console.log(err));
     }
     // user profile
     const profile = <>
         <div className="dropdown dropdown-end">
             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full"
-                data-tooltip-id="my-tooltip"
-                data-tooltip-content={user?.displayName}
-                data-tooltip-place="bottom"
+                    data-tooltip-id="my-tooltip"
+                    data-tooltip-content={user?.displayName}
+                    data-tooltip-place="bottom"
                 >
                     <Tooltip id='my-tooltip'></Tooltip>
                     <img
@@ -80,36 +81,47 @@ const Navbar = () => {
         <li><NavLink to="/" className="text-lg text-white hover:text-yellow-400">Home</NavLink></li>
         <li><NavLink to="/findTutors/all" className="text-lg text-white hover:text-yellow-400">Find Tutors</NavLink></li>
         <li><NavLink to="/contactMe" className="text-lg text-white hover:text-yellow-400">Contact</NavLink></li>
-       {
-        user &&<>
-         <li><NavLink to="/addTutorials" className="text-lg text-white hover:text-yellow-400">Add Tutorials</NavLink></li>
-        <li><NavLink to="/myTutorials" className="text-lg text-white hover:text-yellow-400">My Tutorials</NavLink></li>
-        <li><NavLink to="/myBookedTutors" className="text-lg text-white hover:text-yellow-400">My Booked Tutors</NavLink></li>
-        </>
-       }
+        {
+            user && <>
+                <div className='lg:hidden items-center justify-center text-center *:mb-4'>
+                    <li><NavLink to="/addTutorials" className="text-lg text-white hover:text-yellow-400">Add Tutorials</NavLink></li>
+                    <li><NavLink to="/myTutorials" className="text-lg text-white hover:text-yellow-400">My Tutorials</NavLink></li>
+                    <li><NavLink to="/myBookedTutors" className="text-lg text-white hover:text-yellow-400">My Booked Tutors</NavLink></li>
+                </div>
+                <div className="dropdown hidden lg:block dropdown-hover">
+                    <div tabIndex={0} role="button" className="text-lg flex items-center text-white hover:text-yellow-400 hover:text-[20px] hover:font-bold"> My Panel <MdKeyboardArrowDown className='text-3xl'/></div>
+                    <ul tabIndex={0} className="dropdown-content menu bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 rounded-box z-[1] w-52 p-2 shadow">
+                        <li><NavLink to="/addTutorials" className="text-lg text-white hover:text-yellow-400">Add Tutorials</NavLink></li>
+                        <li><NavLink to="/myTutorials" className="text-lg text-white hover:text-yellow-400">My Tutorials</NavLink></li>
+                        <li><NavLink to="/myBookedTutors" className="text-lg text-white hover:text-yellow-400">My Booked Tutors</NavLink></li>
+                    </ul>
+                </div>
+
+            </>
+        }
         <li className='flex justify-center items-center gap-3'>
 
-            {loading ?  <span className="loading loading-ring loading-lg"></span>
-                :<>
-                {user ?<>
-                    <button
-                        onClick={handleSignout}
-                        to="login"
-                        className="px-4 py-2 bg-yellow-400 text-gray-800 rounded-lg shadow-lg hover:bg-yellow-500"
-                    >
-                        Log Out
-                    </button>
-                    {profile}
-                </>  :
-                <>
-                    <Link
-                        to="login"
-                        className="px-4 py-2 bg-yellow-400 text-gray-800 rounded-lg shadow-lg hover:bg-yellow-500"
-                    >
-                        Sign Up
-                    </Link>
-                </>}
-            </>
+            {loading ? <span className="loading loading-ring loading-lg"></span>
+                : <>
+                    {user ? <>
+                        <button
+                            onClick={handleSignout}
+                            to="login"
+                            className="px-4 py-2 bg-yellow-400 text-gray-800 rounded-lg shadow-lg hover:bg-yellow-500"
+                        >
+                            Log Out
+                        </button>
+                        {profile}
+                    </> :
+                        <>
+                            <Link
+                                to="login"
+                                className="px-4 py-2 bg-yellow-400 text-gray-800 rounded-lg shadow-lg hover:bg-yellow-500"
+                            >
+                                Sign Up
+                            </Link>
+                        </>}
+                </>
             }
 
         </li>
@@ -167,7 +179,7 @@ const Navbar = () => {
                         </button>
                         <div onClick={handleToggle} className='p-2'>
                             {
-                               themeToggle? <FiMoon  className="h-10 bg-[#24292E] text-white rounded-full w-10"/>:<IoIosSunny  className="h-10 bg-yellow-500 text-white rounded-full w-10"/>    
+                                themeToggle ? <FiMoon className="h-10 bg-[#24292E] text-white rounded-full w-10" /> : <IoIosSunny className="h-10 bg-yellow-500 text-white rounded-full w-10" />
                             }
                         </div>
                     </div>
@@ -175,14 +187,14 @@ const Navbar = () => {
                         {links}
                         <div onClick={handleToggle} className='p-2'>
                             {
-                                themeToggle? <FiMoon  className="h-10 bg-[#24292E] text-white rounded-full w-10"/>:<IoIosSunny  className="h-10 bg-yellow-500 text-white rounded-full w-10"/>    
+                                themeToggle ? <FiMoon className="h-10 bg-[#24292E] text-white rounded-full w-10" /> : <IoIosSunny className="h-10 bg-yellow-500 text-white rounded-full w-10" />
                             }
                         </div>
                     </ul>
                 </div>
             </div>
             {isOpen && (
-                <div  className="lg:hidden bg-gradient-to-r from-purple-600 via-pink-600 to-red-600 text-white">
+                <div className="lg:hidden bg-gradient-to-r from-purple-600 via-pink-600 to-red-600 text-white">
                     <ul className="flex flex-col items-center space-y-4 py-4">
                         {links}
                     </ul>
